6457578a4986b99702904285b7995de3a578d170
Vy Tran <thuyvy300422@gmail.com>
2025-02-14 15:02:31 +0700

    add exercise trie

---
+++ draw_trie.py
@@ -0,0 +1,118 @@
+words = ["cat", "banana", "obama", "car", "cow", "alibaba"]
+
+children = {
+    "c":{
+        "children":{
+            "a":{
+                "children":{
+                    "t":{
+                        "children":{},
+                        "is_end_of_word": True
+                    },
+                    "r":{
+                        "children":{},
+                        "is_end_of_word": True
+                    }
+                    },
+                "is_end_of_word": False
+            },
+            "o":{
+                "children":{
+                    "w":{
+                        "children":{},
+                        "is_end_of_word": True
+                    }
+                    },
+                "is_end_of_word": False
+            }
+            },
+        "is_end_of_word": False
+    },
+    "b":{
+        "children":{
+            "a":{
+                "children":{
+                    "n":{
+                        "children":{
+                            "a":{
+                                "children":{
+                                    "n":{
+                                        "children":{
+                                            "a":{
+                                                "children":{},
+                                                "is_end_of_word": True
+                                            }
+                                            },
+                                        "is_end_of_word": False
+                                    }
+                                    },
+                                "is_end_of_word": False
+                            }
+                            },
+                        "is_end_of_word": False
+                    }
+                    },
+                "is_end_of_word": False
+            }
+            },
+        "is_end_of_word": False
+    },
+    "o":{
+        "children":{
+            "b":{
+                "children":{
+                    "a":{
+                        "children":{
+                            "m":{
+                                "children":{
+                                    "a":{
+                                        "children":{},
+                                        "is_end_of_word": True
+                                    }
+                                    },
+                                "is_end_of_word": False
+                            }
+                            },
+                        "is_end_of_word": False
+                    }
+                    },
+                "is_end_of_word": False
+            }
+            },
+        "is_end_of_word": False
+        },
+    "a":{
+        "children":{
+            "l":{
+                "children":{
+                    "i":{
+                        "children":{
+                            "b":{
+                                "children":{
+                                    "a":{
+                                        "children":{
+                                            "b":{
+                                                "children":{
+                                                    "a":{
+                                                        "children":{},
+                                                        "is_end_of_word": True
+                                                    }
+                                                    },
+                                                "is_end_of_word": False
+                                            }
+                                            },
+                                        "is_end_of_word": False
+                                    }
+                                    },
+                                "is_end_of_word": False
+                            }
+                            },
+                        "is_end_of_word": False
+                    }
+                    },
+                "is_end_of_word": False
+            }
+            },
+        "is_end_of_word": False
+    },
+}
